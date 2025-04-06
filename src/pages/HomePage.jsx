2c3689285b108dfcbef3cc1588bb9367318e2359
Jared <jared.jl.lim@gmail.com>
2025-04-05 19:18:29 -0700

    please

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
 
     return (
       <div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
-        <h1 className="mb-4">Welcome to Up-Grade!</h1>
+        <h1 className="mb-4">Welcome to Up-Grade</h1>
         <button className="btn btn-primary" onClick={() => navigate("/SelectPage")}>Upload Study Materials</button>
         <Link to="/TestPage">Test Page</Link>
         <Link to="/OCRPage">OCR Page</Link>

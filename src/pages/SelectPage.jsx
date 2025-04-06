f2a09fd3a73755613244b0628a0de53d856126af
DonovanThach <tehlamo0528@gmail.com>
2025-04-05 20:33:40 -0700

    got rid of template

--- src/pages/SelectPage.jsx
+++ src/pages/SelectPage.jsx
@@ -6,28 +6,19 @@ export default function SelectPage() {
         {/*Navbar for the website using bootstrap */}
         <nav class = "navbar navbar-expand-lg fixed-top" style = {{backgroundColor: /*'#dceed1'*/ '#503d3f'}} data-bs-theme = "light">
             <div class = "container-fluid">
-                <Link to = "/" class = "navbar-brand" style = {{color: '#FED8E9'}}>Up-Grade</Link>
+                <Link to = "/quizMe" class = "navbar-brand" style = {{color: '#FED8E9'}}>Up-Grade</Link>
                 <div class = "collapse navbar-collapse" id = "navbarNav">
                     <ul class = "navbar-nav">
                         <li class = "class-item">
-                            <Link to = "/SelectPage" class = "nav-link active" style = {{color: '#FED8E9'}}>Home</Link>
-                        </li>
-                        <li class = "class-item">
-                            <Link to = "/" class = "nav-link active" style = {{color: '#FED8E9'}}>Text</Link>
-                        </li>
-                        <li class = "class-item">
-                            <Link to = "/OCRPage" class = "nav-link active" style = {{color: '#FED8E9'}}>Upload</Link>
-                        </li>
-                        <li class = "class-item">
-                            <Link to = "/" class = "nav-link active" style = {{color: '#FED8E9'}}>Photo</Link>
+                            <Link to = "/QuizPage" class = "nav-link active" style = {{color: '#FED8E9'}}>Upload</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
 
-        {/*The main body and structure of the page using bootstrap */}
-        <div class = "position-relative" style = {{ height: "200vh", width: "100%" }}>
+        {/*The main body and structure of the page using bootstrap*/}
+        <div class = "position-relative" style = {{ height: "200vh", width: "100%" }}>{/*}
             <div class = "position-absolute start-50 translate-middle" style = {{top: "8%"}}>
                 <div class = "row justify-content-end align-items-center">
                     <div class = "col">
@@ -36,7 +27,7 @@ export default function SelectPage() {
                 </div>
             </div>
 
-            {/*Div for the Upload section */}
+            {/*Div for the Upload section 
             <div class = "position-relative" style = {{top: "40vh", height: "40vh", width: "90%", backgroundColor: "lightBlue", left: "5%"}}>
                 <div class = "position-absolute translate-middle" style = {{top: "15%", left: "20%"}}>
                     <p class = "text-center fs-4 fw-bold"> Input Your Text </p>
@@ -49,7 +40,7 @@ export default function SelectPage() {
                 <div class = "position-absolute translate-middle" style = {{top : "40%", right: "7%"}}>
                     <p class = "text-center fs-4 fw-bold"> Try It Now! </p>
                     <div class = "position-absolute start-50 translate-middle" style = {{top: "150%"}}>
-                        <Link to = "/" class = "btn btn-dark">Upload</Link>
+                        <Link to = "/QuizPage" class = "btn btn-dark">Upload</Link>
                     </div>
                 </div>
             </div>
